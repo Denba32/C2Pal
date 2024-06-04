37c5dc959e335a76adebc93d2c90e41e764e134e
birdham <139436177+birdham@users.noreply.github.com>
2024-06-04 17:46:04 +0900

    >>플레이어 이동구현 및 상태 구현

---
+++ Assets/Jiwoon/Scripts/Player/PlayerConditions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UIConditions uiconditions;
+
+    Condition health { get { return uiconditions.health; } }
+    Condition hunger { get { return uiconditions.hunger; } }
+    Condition stamina { get { return uiconditions.stamina; } }
+
+    public float noHungerHealthDecay;
+    void Update()
+    {
+        hunger.Substract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue <= 0f)
+        {
+            health.Substract(health.passiveValue * Time.deltaTime);
+        }
+
+        if(health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("당신은 죽었습니다!");
+    }
+}
