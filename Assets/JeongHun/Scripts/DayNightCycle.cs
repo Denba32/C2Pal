5bc2e14e96049c0d3513d0b1d36d6a3d2df2110b
whew15 <huna94@naver.com>
2024-06-07 16:33:39 +0900

    update :  Cave Castle Scenes
    메인 맵과 던전, 성 업데이트

--- Assets/JeongHun/Scripts/DayNightCycle.cs
+++ Assets/JeongHun/Scripts/DayNightCycle.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    [Range(0.0f, 1.0f)]
+    [Range(0.0f, 24.0f)]
     public float time;
-    public float fullDayLength ;
-    public float startTime = 0.4f;
+    public float fullDayLength = 1200f;
+    public float startTime = 10.0f;
     private float timeRate;
     public Vector3 noon;
 
@@ -21,37 +21,37 @@ public class DayNightCycle : MonoBehaviour
     public Gradient moonColor;
     public AnimationCurve moonIntensity;
 
-
     [Header("Other Lighting")]
     public AnimationCurve lightingIntensityMultiplier;
     public AnimationCurve reflectionIntensityMuliplier;
 
     void Start()
     {
-        timeRate = 1.0f / fullDayLength;
+        timeRate = 24.0f / fullDayLength;
         time = startTime;
     }
 
     void Update()
     {
-        time = (time + timeRate * Time.deltaTime) % 1.0f;
+        time = (time + timeRate * Time.deltaTime) % 24.0f;
         UpdateLighting(sun, sunColor, sunIntensity);
         UpdateLighting(moon, moonColor, moonIntensity);
 
-        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
-        RenderSettings.reflectionIntensity = reflectionIntensityMuliplier.Evaluate(time);
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time / 24.0f);
+        RenderSettings.reflectionIntensity = reflectionIntensityMuliplier.Evaluate(time / 24.0f);
     }
 
     void UpdateLighting(Light lightSource, Gradient gradient, AnimationCurve intensityCurve)
     {
-        float intensity = intensityCurve.Evaluate(time);
+        float normalizedTime = time / 24.0f;
+        float intensity = intensityCurve.Evaluate(normalizedTime);
 
-        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4f;
-        lightSource.color = gradient.Evaluate(time);
+        lightSource.transform.eulerAngles = (normalizedTime - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4f;
+        lightSource.color = gradient.Evaluate(normalizedTime);
         lightSource.intensity = intensity;
 
         GameObject go = lightSource.gameObject;
-        if(lightSource.intensity == 0&& go.activeInHierarchy)
+        if (lightSource.intensity == 0 && go.activeInHierarchy)
         {
             go.SetActive(false);
         }
