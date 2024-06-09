0f7cb899be40d277897fdab7504667458ce57369
whew15 <huna94@naver.com>
2024-06-09 17:27:25 +0900

    Add : Bgm

---
+++ Assets/Contents/BGM/BgmLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmLoop : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Space를 누른 경우 BGM loop On/Off
+        if (Input.GetKeyDown(KeyCode.Space))
+            audioSource.loop = !audioSource.loop;
+
+        /* Space를 누른 경우 BGM mute On/Off
+        if (Input.GetKeyDown(KeyCode.Space))
+            audioSource.mute = !audioSource.mute;
+        */
+
+    }
+}
